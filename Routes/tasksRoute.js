534cade89e466ded38183d61776eaa71b5f4c6b2
AkashDhere18 <dhereakash18@gmail.com>
2026-06-18 23:10:52 +0530

    created api for updateTask, deleteTask,getTaskbyStatus,getTaskbySelected,Month and added authorization

--- Routes/tasksRoute.js
+++ Routes/tasksRoute.js
@@ -1,20 +1,26 @@
 const express = require('express')
+const {auth,admin} = require('../middleware/auth')
 
 const {createTask,
     getAllTask,
     getByID,
     updateStatus,
     updateTask,
-    deleteTask} = require('../controllers/taskControllers')
+    deleteTask,
+    getByStatus,
+    getByMonth} = require('../controllers/taskControllers')
 
 const router = express.Router()
 
-router.post('/create', createTask)
-router.get('/getAllTask', getAllTask)
-router.get('/getTaskById/:ID', getByID)
-router.patch('/updateStatus/:ID', updateStatus)
-router.put('/updateTask/:ID', updateTask)
-router.delete('/deleteTask/:ID', deleteTask)
+router.post('/create',auth,admin, createTask)
+router.get('/getAllTask', auth,getAllTask)
+router.get('/getTaskById/:ID',auth, getByID)
+router.patch('/updateStatus/:ID',auth, updateStatus)
+router.put('/updateTask/:ID',auth,admin, updateTask)
+router.delete('/deleteTask/:ID', auth, admin, deleteTask)
+
+router.get('/getTaskByStatus/search' , getByStatus)
+router.get('/getTaskBySelectedMonth/search', getByMonth)
 
 
 module.exports = router
--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -1,5 +1,6 @@
 const {sequelize} = require('../Config/db')
 const Task = require('../Modals/taskModal')
+const { Op, fn, col } = require('sequelize');
 
 
 async function createTask(req,res){
@@ -49,7 +50,25 @@ async function getByID(req,res){
 }
 
 async function updateStatus(req,res){
+    const ID = req.params.ID
+    const status = req.body.status
     try {
+        const statusArray = ["Pending", "Inprogress", "Completed"]
+
+        if(!statusArray.includes(status)){
+            res.status(400).send({msg:"Data not found",succese:false})
+        }
+
+        let taskToUpdateStatus = await Task.findByPk(ID);
+
+        if(!taskToUpdateStatus){
+            res.status(400).send({msg:"Task not found",succese:false})
+            
+        }
+
+        await taskToUpdateStatus.update({status:status})
+        res.status(200).send({msg:"Task updated succesfully",succese:true})
+
         
     } catch (error) {
         console.log(error)
@@ -58,8 +77,25 @@ async function updateStatus(req,res){
 }
 
 async function updateTask(req,res){
+    const ID = req.params.ID
+
     try {
-        
+        const taskForUpdate = await Task.findByPk(ID)
+
+        if(!taskForUpdate){
+            res.status(400).send({msg:"Task not found",succese:false})
+        }
+
+        await taskForUpdate.update({
+            title:req.body.title || taskForUpdate.title,
+            description:req.body.description || taskForUpdate.description,
+            startDate:req.body.startDate || taskForUpdate.startDate,
+            endDate:req.body.endDate || taskForUpdate.endDate
+        })
+
+        res.status(200).send({msg:"Task updated succesfully",succese:true})
+
+
     } catch (error) {
         console.log(error)
         res.status(500).send({msg:"Server error", succese:false})
@@ -67,13 +103,72 @@ async function updateTask(req,res){
 }
 
 async function deleteTask(req,res){
+
+    const ID = req.params.ID
+
     try {
+
+        const taskForDelete = await Task.findByPk(ID)
+
+        if(!taskForDelete){
+            res.status(400).send({msg:"Task not found",succese:false})
+        }
+
+        await taskForDelete.destroy()
+
+        res.status(200).send({msg:"Task deleted succesfully",succese:true})
+
+
         
     } catch (error) {
         console.log(error)
         res.status(500).send({msg:"Server error", succese:false})
     }
 }
+
+async function getByStatus(req,res){
+    try {
+        const status = req.query.status
+
+        const bystatus = await Task.findAll({where:{status:status}})
+
+        if(bystatus.length == 0){
+            return res.status(400).send({msg:"Tasks Not found",succese:false})
+        }
+
+        res.status(200).send({succese:true,bystatus:bystatus})
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({msg:"Server error", succese:false})
+    }
+}
+
+async function getByMonth(req,res){
+    try {
+        const month = Number(req.query.month)
+      
+        const byMonth = await Task.findAll({
+            where: sequelize.where(
+                fn('MONTH' , col('startDate')) , month
+            )
+    })
+
+    if(byMonth.length == 0){
+        return res.status(400).send({msg:"Tasks Not found",succese:false})
+    }
+
+    res.status(200).send({succese:true,byMonth:byMonth})
+
+    
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({msg:"Server error", succese:false})
+    }
+}
+
+
 
 
 
@@ -85,6 +180,8 @@ module.exports = {
     getByID,
     updateStatus,
     updateTask,
-    deleteTask    
+    deleteTask,
+    getByStatus,
+    getByMonth    
 }
 
